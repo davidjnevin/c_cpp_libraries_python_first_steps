f0a3d427b992b63176eabb2cef93a833896575ec
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-07-11 18:19:47 +0200

    Add array_sum c library and clean up some imports

--- ctypes_tutorial.py
+++ ctypes_tutorial.py
@@ -1,11 +1,11 @@
 import ctypes
+import os
 
 # Passing and returning c_char_p strings
 
 print("=" * 20)
-clibrary = ctypes.CDLL(
-    "/Users/Communitymanager-work/Google Drive/code/ctypes_tutorial/clibrary.so"
-)
+path = os.getcwd()
+clibrary = ctypes.CDLL(os.path.join(path, "clibrary.so"))
 
 func = clibrary.display
 func.argtypes = [ctypes.c_char_p, ctypes.c_int]  # Many args possible
@@ -16,9 +16,7 @@ func(b"David", 46)
 
 print("\n")
 print("=" * 20)
-clibrary_add = ctypes.CDLL(
-    "/Users/Communitymanager-work/Google Drive/code/ctypes_tutorial/clibrary_add.so"
-)
+clibrary_add = ctypes.CDLL(os.path.join(path, "clibrary_add.so"))
 
 func_2 = clibrary_add.add
 func_2.argtypes = [ctypes.c_int, ctypes.c_int]  # Many args possible
@@ -36,9 +34,7 @@ print("\n")
 print("=" * 20)
 print("\n")
 
-clibrary_pointers = ctypes.CDLL(
-    "/Users/Communitymanager-work/Google Drive/code/ctypes_tutorial/clibrary_pointers.so"
-)
+clibrary_pointers = ctypes.CDLL(os.path.join(path, "clibrary_pointers.so"))
 
 alloc_func = clibrary_pointers.alloc_memory
 alloc_func.restype = ctypes.POINTER(ctypes.c_char_p)
@@ -51,3 +47,41 @@ cstring_pointer = alloc_func()
 cstring = ctypes.c_char_p.from_buffer(cstring_pointer)
 print(cstring.value)
 free_func(cstring_pointer)
+
+# Using pointers
+
+print("=" * 20)
+print("\n")
+
+num = ctypes.c_int(100)
+ptr = ctypes.pointer(num)
+
+print(ptr.contents)
+
+## Using factory function to create a new ctypes type.
+
+ptr2 = ctypes.POINTER(ctypes.c_int)
+
+ptr2.contents = num
+print(ptr2.contents)
+
+# ctypes.POINTER(type): This function is a factory function that creates a new ctypes type. It doesn't create a pointer; it creates a new type that can represent pointers to a particular other type.
+# ctypes.pointer(obj): This function actually creates a pointer to an existing ctypes object. For example, if i is a ctypes.c_int instance, then ctypes.pointer(i) returns a pointer to i.
+
+# Using Arrays
+# Use an array sum library written in c
+
+print("=" * 20)
+print("\n")
+
+# create and array of c_ints, default value is zero and then set to 0, 1, 2, 3, ...
+
+values = (ctypes.c_int * 10)()
+for i in range(len(values)):
+    values[i] = i
+
+path = os.getcwd()
+clibrary_array_sum = ctypes.CDLL(os.path.join(path, "clibrary_array_sum.so"))
+
+func = clibrary_array_sum.sumArray
+# func.argtypes = [ctype.c_int]
